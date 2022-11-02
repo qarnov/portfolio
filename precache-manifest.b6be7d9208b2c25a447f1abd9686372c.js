21d2e06f15b775f9b4f07e55383c545d5da9dde1
qarnov <afdhalrahman26.com>
2022-11-02 21:46:24 +0530

    first commit

---
+++ precache-manifest.b6be7d9208b2c25a447f1abd9686372c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c35b9b413293f6fe85bf8096f04210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "237b7c13d728ab53d495",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "578cf60529e10da066db",
+    "url": "/static/css/main.b5800f66.chunk.css"
+  },
+  {
+    "revision": "237b7c13d728ab53d495",
+    "url": "/static/js/2.1e7885a8.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.1e7885a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578cf60529e10da066db",
+    "url": "/static/js/main.d92a3de1.chunk.js"
+  },
+  {
+    "revision": "0c57f973157196477324",
+    "url": "/static/js/runtime-main.6b5ef6cf.js"
+  }
+]);
